d27950edca0c19a7fbcf11cbc2ca7d450690ac51
Jake Greenbaum <jakegreenbaum@umail.ucsb.edu>
2023-04-12 20:21:02 -0700

    jg - fixed duplicate of the routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,6 @@ import SanFranciscoPage from "main/pages/SanFranciscoPage";
 import ValenciaPage from "main/pages/ValenciaPage";
 import LagunaBeachPage from "main/pages/LagunaBeachPage";
 
-
 import "bootstrap/dist/css/bootstrap.css";
 import CopenhagenPage from "main/pages/CopenhagenPage";
 
@@ -19,20 +18,19 @@ function App() {
         <Route exact path="/" element={<HomePage />} />
         <Route exact path="/towns/AvilaBeach" element={<AvilaBeachPage />} />
         <Route exact path="/towns/LosAlamos" element={<LosAlamosPage />} />
+
         <Route
           exact
           path="/towns/ArroyoGrande"
           element={<ArroyoGrandePage />}
         />
+        <Route exact path="/towns/Copenhagen" element={<CopenhagenPage />} />
+        <Route exact path="/towns/LosAngeles" element={<LosAngelesPage />} />
         <Route
           exact
           path="/towns/SanFrancisco"
           element={<SanFranciscoPage />}
         />
-        <Route exact path="/towns/ArroyoGrande" element={<ArroyoGrandePage />} />
-        <Route exact path="/towns/Copenhagen" element={<CopenhagenPage />} />
-        <Route exact path="/towns/LosAngeles" element={<LosAngelesPage />} />
-        <Route exact path="/towns/SanFrancisco" element={<SanFranciscoPage />} />
         <Route exact path="/towns/Valencia" element={<ValenciaPage />} />
         <Route exact path="/towns/LagunaBeach" element={<LagunaBeachPage />} />
       </Routes>
