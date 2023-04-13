cbaad6c0789aadb773956d982c328fc51b082ee3
Alex Granados <alexg@169-231-149-193.wireless.ucsb.edu>
2023-04-12 18:29:51 -0700

    AG - add page for Valencia

---
+++ frontend/src/main/pages/ValenciaPage.js
@@ -0,0 +1,17 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+export default function ValenciaPage() {
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Valencia, CA</h1>
+        <p>
+         Valencia, California is a medium-sized town in Northern 
+         Los Angeles County.  It is located about forty minutes north of Los Angeles, and about fifty minutes south-east of Ventura. 
+         It is known for being the home of Six Flags Magic Mountain.
+        </p>
+        <p>AG</p>
+      </div>
+    </BasicLayout>
+  )
+}
