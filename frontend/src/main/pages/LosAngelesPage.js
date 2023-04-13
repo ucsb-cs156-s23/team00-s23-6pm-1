1a7c27b022e3c93ca4dbb1806b98160d172e88d4
Max Fletcher <mfletcher@umail.ucsb.edu>
2023-04-12 18:31:15 -0700

    Add LA description

---
+++ frontend/src/main/pages/LosAngelesPage.js
@@ -0,0 +1,28 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+export default function LosAngelesPage() {
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Los Angeles, CA</h1>
+        <p>
+        Los Angeles (L.A.), officially the City of Los Angeles, is a city in Southern California, 
+        in the United States. It has a population of over **3,847,400** people living in the city and over **18 million** 
+        people in the L.A. region. The city covers a total area of **502.7 square miles**⁴ and is built on a fault line 
+        which runs through downtown. Due to this, it has few skyscrapers and tall structures and is 
+        one of the most spread out cities in the world.
+
+        Some famous landmarks in Los Angeles include the Hollywood Walk of Fame, Griffith Observatory and Griffith Park, 
+        TCL Chinese Theatre, Walt Disney Concert Hall, The La Brea Tar Pits and Museum, LACMA, Capitol Records Building, 
+        The Getty, Union Station and Dolby Theater. 
+
+        Los Angeles has one of the best public transportation networks in the country including subways, 
+        light-rail, buses and shuttles to nearly every corner of the Greater Los Angeles area. 
+        The TAP card connects most of the county bus lines, city-specific buses and shuttle options 
+        including LADOT, Santa Monica Big Blue Bus, LAX buses, Santa Clarita Transit and Long Beach Transit.
+        </p>
+        <p>Added by Max F.</p>
+      </div>
+    </BasicLayout>
+  )
+}
